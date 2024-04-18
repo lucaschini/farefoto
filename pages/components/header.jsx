af2d41cb1aa22a9fe6e7722ced4c2ee5fc25b90c
Lucas Chini <lucaschinimendonca@protonmail.com>
2024-04-17 22:43:21 -0300

    primeiras alterações

---
+++ pages/components/header.jsx
@@ -0,0 +1,12 @@
+import Image  from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex justify-center items-center min-h-[92px] gap-32 p-10 font-semibold text-xl">
+      <Link href="/catalogo">Catálogo</Link>
+      <Image src='/Logo.png' width={234} height={92}/>
+      <Link href="/contato">Contato</Link>
+    </div>
+  );
+}
---
+++ pages/components/hero.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+
+export default function Hero(){
+    return(
+        <>
+        <Image src='/Polaroid frente.png' width={100} height={100} className="absolute"/>
+        </>
+    )
+}
