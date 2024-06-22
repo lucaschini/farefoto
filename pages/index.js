05ea3cd7c9ae96fa23b3ef0efd8e356a96a8cf66
Lucas Chini <lucaschinimendonca@protonmail.com>
2024-06-21 23:45:17 -0300

    test: header import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import Header from "./components/Header";
+import Header from "../pages/components/Header";
 import Hero from "./components/Hero";
 import Oferta from "./components/Oferta";
 import Loc from "./components/Loc";
