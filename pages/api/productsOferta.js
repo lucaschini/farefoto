382820e69f082c234d4b324e2250a9058f37eb43
Lucas Chini <lucaschinimendonca@protonmail.com>
2024-06-28 15:07:18 -0300

    feat: added page contato

--- pages/api/productsOferta.js
+++ pages/api/productsOferta.js
@@ -4,20 +4,20 @@ const productsOferta = [
     {
         id: 1,
         category: "revelacoes",
-        title: "Fotos 10x15",
+        title: "Pacote de 100 fotos 10x15",
         content: `Imortalize seus momentos favoritos em uma foto 10x15! Escolha entre o acabamento brilhante para um brilho vibrante ou o acabamento fosco para uma elegância sutil. Capture memórias que durarão para sempre.`,
-        price: "R$ 2,50",
+        price: "R$ 1,30 cada",
         img: "/foto-card.jpeg",
-        link: "https://wa.link/gcaqtx"
+        link: `https://wa.me/+5511953147703/?text=Olá, gostaria de fazer um pedido de`
     },
     {
         id: 2,
         category: "revelacoes",
-        title: "Fotos 10x15",
+        title: "Pacote de 200 fotos 10x15",
         content: `Imortalize seus momentos favoritos em uma foto 10x15! Escolha entre o acabamento brilhante para um brilho vibrante ou o acabamento fosco para uma elegância sutil. Capture memórias que durarão para sempre.`,
-        price: "R$ 2,50",
+        price: "R$ 1,10 cada",
         img: "/foto-card.jpeg",
-        link: "https://wa.link/gcaqtx"
+        link: `https://wa.me/+5511953147703/?text=Olá, gostaria de fazer um pedido de`
     },
     {
         id: 3,
@@ -26,7 +26,7 @@ const productsOferta = [
         content: `Imortalize seus momentos favoritos em uma foto 10x15! Escolha entre o acabamento brilhante para um brilho vibrante ou o acabamento fosco para uma elegância sutil. Capture memórias que durarão para sempre.`,
         price: "R$ 2,50",
         img: "/foto-card.jpeg",
-        link: "https://wa.link/gcaqtx"
+        link: `https://wa.me/+5511953147703/?text=Olá, gostaria de fazer um pedido de`
     },
    
 ];
--- pages/contato.js
+++ pages/contato.js
@@ -1,13 +1,33 @@
 import Header from "@/components/Header";
 
-export default function Contato (){
-    return(
-        <>
-        <Header/>
-        <div className="w-full flex flex-col items-center mt-5">
-            <h1 className="text-5xl font-bold">Atendimento ao cliente:</h1>
+export default function Contato() {
+  return (
+    <>
+      <Header/>
+      <div className="h-[80vh] flex flex-col items-center justify-center mt-10">
+        <div className="w-[90%] lg:max-w-[70%] bg-white text-black rounded-xl p-6 lg:py-10 m-4 flex flex-col justify-center items-center gap-6 lg:gap-10">
+          <h2 className="text-3xl lg:text-5xl font-bold text-center">
+            Horário de funcionamento:
+          </h2>
+          <p className="text-xl lg:text-2xl text-center">Segunda a Sexta das 9h às 18h<br/>Sábado das 9h às 13h</p>
+          <h2 className="text-3xl lg:text-5xl font-bold text-center">
+            Atendimento ao cliente:
+          </h2>
+          <div className="flex flex-col lg:flex-row gap-3 items-center justify-center text-lg lg:text-2xl">
+            <p>Whatsapp - (11) 95314-7703</p>
+            <a href="https://wa.me/+5511953147703/?text=Olá">
+              <btn className='px-4 py-3 bg-orange-400 rounded-lg font-semibold text-white flex items-center justify-center'>📞 Envie uma mensagem!</btn>
+            </a>
+          </div>
+          <div className="flex flex-col lg:flex-row gap-3 items-center justify-center text-lg lg:text-2xl">
+            <p>Whatsapp - (11) 94327-8453</p>
+            <a href="https://wa.me/+55119432784533/?text=Olá">
+              <btn className='px-4 py-3 bg-orange-400 rounded-lg font-semibold text-white flex items-center justify-center'>📞 Envie uma mensagem!</btn>
+            </a>
+          </div>
+          <h2 className="text-xl lg:text-2xl text-center">Email: farefoto1@gmail.com</h2>
         </div>
-        
-        </>
-    )
+      </div>
+    </>
+  );
 }
