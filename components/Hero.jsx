9bbb13880ddbb77c525f50c540510da2f8a9751c
Lucas Chini <lucaschinimendonca@protonmail.com>
2024-11-16 09:45:02 -0300

    feat: new hero and sections

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center bg-red-700 h-[52vh] p-6 md:p-0">
-        <div className="md:w-[500px] md:h-[500px]">
-            <Image src="/dia_dos_pais.png" width={400} height={400} layout="responsive" alt="Dia dos pais" className="aspect-square"/>
+    <div className="flex flex-col md:flex-row items-center justify-center bg-red-700 md:h-[52vh] p-6 md:p-0 py-10 gap-11">
+        <div className="md:w-[500px] md:h-[500px] w-[300px] h-[300px]">
+            <Image src="/img_hero.webp" width={400} height={400} layout="responsive" alt="Dia dos pais" className="aspect-square"/>
         </div>
-        {/* <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-10">
             <div>
-            <Image src="/caneca.svg" width={300} height={300} layout="responsive" alt="Dia dos pais"/>
+            <h1 className="md:text-4xl text-2xl font-bold text-white text-center">🎄 Celebre o Natal com Memórias Inesquecíveis! 📸</h1>
             </div> 
-            <Link href='/'><button className="px-4 py-3 shadow-xl text-orange-400 bg-white font-bold rounded-lg text-xl w-fit hover:bg-orange-400 hover:text-white hover:outline-white hover:outline hover:outline-4">Confira nossos produtos</button></Link>
-        </div> */}
+            <Link href='/'><button className="px-4 py-3 shadow-xl text-red-700 bg-white font-bold rounded-lg text-xl w-fit hover:bg-red-700 hover:text-white hover:outline-white hover:outline hover:outline-4">Confira nossos produtos</button></Link>
+        </div>
     </div>
   );
 }
